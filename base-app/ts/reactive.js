5593c774ab31fa773fb29e5e0010e4ee2acd54c1
clara fernandez <clara.fernandez@babel.es>
2018-04-09 12:10:32 +0200

    reactive js

--- base-app/ts/reactive.js
+++ base-app/ts/reactive.js
@@ -6,6 +6,7 @@ var Rx = require('rxjs/Rx');
 
 let aDatos=[1,2,3,4,5,6];
 
+//Se disparan 3 eventos: 3 por los que pasan el filtro y el completed.
 const observable = Rx.Observable
 .from(aDatos)
 .map( x => x*x )
@@ -17,8 +18,17 @@ observable.subscribe(
     function onCompleted(){console.log('Completed')}
 );
 
-//Rx.Observer.create([onNext], [onError], [onCompleted])
+aDatos.push(7);
 
+observable.subscribe(
+    function onNext(x) { console.log('Siguiente ' + x) },
+    function onError(err) {console.log('Error: ' + err)},
+    function onCompleted(){console.log('Se acabó')}
+);
+
+//Rx.Observer.create([onNext], [onError], [onCompleted])
+//El observable va cambiando de valor y va diciendo cuánto vale.
+//En el observer definimos 3 tipos de eventos que van respondiendo al observable.
 /* const observador = Rx.Observer.create(
     function onNext(x) { console.log('Next ' + x) },
     function onError(err) {console.log('Error: ' + err)},
